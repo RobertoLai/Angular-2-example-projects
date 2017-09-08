f3c2bc8a878cfc71cbc36ef4312f3bf633eefa5f
Roberto Lai <rl@datawerks.com>
2017-09-08 13:51:15 +0200

    package.json modified

--- prj_1_simple_website/app/components/pages/subscribe.component.js
+++ prj_1_simple_website/app/components/pages/subscribe.component.js
@@ -8,8 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require("@angular/core");
-var post_1 = require("../../model/post");
+var core_1 = require('@angular/core');
+var post_1 = require('../../model/post');
 var SubscribeComponent = (function () {
     function SubscribeComponent() {
         this.model = new post_1.Post(1, '', '', '');
@@ -21,16 +21,16 @@ var SubscribeComponent = (function () {
     SubscribeComponent.prototype.submitSubscription = function () {
         this.noJoke = false;
     };
+    SubscribeComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'subscribe',
+            templateUrl: 'subscribe.component.html',
+            styles: ["\n    form .ng-dirty {}\n    form .ng-pristine {}\n    form .ng-valid[required] {border: 2px solid #42a948;}\n    form .ng-invalid {border: 2px solid #a94442;}\n    "]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], SubscribeComponent);
     return SubscribeComponent;
 }());
-SubscribeComponent = __decorate([
-    core_1.Component({
-        moduleId: module.id,
-        selector: 'subscribe',
-        templateUrl: 'subscribe.component.html',
-        styles: ["\n    form .ng-dirty {}\n    form .ng-pristine {}\n    form .ng-valid[required] {border: 2px solid #42a948;}\n    form .ng-invalid {border: 2px solid #a94442;}\n    "]
-    }),
-    __metadata("design:paramtypes", [])
-], SubscribeComponent);
 exports.SubscribeComponent = SubscribeComponent;
 //# sourceMappingURL=subscribe.component.js.map
